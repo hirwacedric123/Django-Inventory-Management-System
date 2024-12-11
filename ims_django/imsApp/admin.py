74a14aec6ca5ffed29b3723281a431df1e6f0293
hirwa cedric <hirwacedr12@gmail.com>
2024-12-11 17:49:19 +0200

    adding list display in dashboard

--- ims_django/imsApp/admin.py
+++ ims_django/imsApp/admin.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
 from imsApp.models import Category, Product, Stock, Invoice, Invoice_Item
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Stock)
-admin.site.register(Invoice)
-admin.site.register(Invoice_Item)
+# admin.site.register(Category)
+# admin.site.register(Product)
+# admin.site.register(Stock)
+# admin.site.register(Invoice)
+# admin.site.register(Invoice_Item)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'status', 'date_created', 'date_updated')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'code', 'name', 'price', 'status', 'date_created', 'date_updated')
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'quantity', 'type', 'date_created', 'date_updated')
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'transaction', 'customer', 'total', 'date_created', 'date_updated')
+
+@admin.register(Invoice_Item)
+class InvoiceItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'invoice', 'product', 'stock', 'price', 'quantity')
